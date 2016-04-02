30e6b3a00402241215a7b27bb31695f021a99f51
Eric Ihli <ericihli@gmail.com>
2016-04-02 01:05:42 -0700

    Add initial test

---
+++ tests/largest-prime-factor-test.js
@@ -0,0 +1,9 @@
+var expect = require('chai').expect;
+
+describe('isPrime', function() {
+  it('returns true if num is prime', function() {
+  });
+});
+
+describe('finding largest prime factor', function() {
+});
